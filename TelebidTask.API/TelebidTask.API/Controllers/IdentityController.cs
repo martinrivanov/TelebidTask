41d9de2b978c3cb7e06029d6e0456d2ba0bcfd45
martinrivanov <martinrivanov43@gmail.com>
2024-06-26 15:32:10 +0300

    implementing cookie auth

--- TelebidTask.API/TelebidTask.API/Controllers/IdentityController.cs
+++ TelebidTask.API/TelebidTask.API/Controllers/IdentityController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.JsonPatch;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using TelebidTask.Data.Models;
 using TelebidTask.Services.Contracts;
 
@@ -16,6 +20,7 @@ namespace TelebidTask.API.Controllers
             this.userService = userService;
         }
 
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
         [HttpGet]
         [Route("/{id}")]
         public async Task<IActionResult> GetUserById(Guid id)
@@ -30,6 +35,7 @@ namespace TelebidTask.API.Controllers
             return new OkObjectResult(user);
         }
 
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
         [HttpPatch]
         [Route("/")]
         public async Task<IActionResult> UpdateUser(Guid id, [FromBody]JsonPatchDocument<User> patch)
@@ -84,7 +90,25 @@ namespace TelebidTask.API.Controllers
                 return new BadRequestObjectResult(new { Message = "Incorrect email or password" });
             }
 
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Name),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
             return new OkObjectResult(new { UserId = user.Id });
+        }
+
+        [HttpPost]
+        [Route("/Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return new OkResult();
         }
     }
 }
--- TelebidTask.API/TelebidTask.API/Program.cs
+++ TelebidTask.API/TelebidTask.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using TelebidTask.Data.Contracts;
 using TelebidTask.Data.DatabaseRepository;
 using TelebidTask.Services.Contracts;
@@ -7,6 +8,14 @@ using TelebidTask.Services.UserService;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opt =>
+    {
+        opt.SlidingExpiration = true;
+        opt.ExpireTimeSpan = new TimeSpan(0, 20, 0);
+    });
+
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
